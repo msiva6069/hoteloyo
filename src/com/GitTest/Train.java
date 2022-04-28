92f8e0c7693d207f98dd7bfeed86f4a6c997fd7f
msiva6069 <msiva6069@gmail.com>
2022-04-28 18:45:34 +0530

    New

---
+++ src/com/GitTest/Train.java
@@ -0,0 +1,19 @@
+package com.GitTest;
+
+public class Train {
+	
+
+	public static void main(String[] args) {
+		
+		int n=4;
+		int fact=1;
+		
+		for (int i = 1; i <= n; i++) {
+			
+			fact =fact *i;
+			
+		}
+		System.out.println(fact);
+	}
+
+}
